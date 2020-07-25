e4f2981e09d163048d8a5fa9b0d3179606939506
Roman Stadler <rosta99x@gmx.ch>
2020-07-25 11:48:28 +0200

    Update mailScraper.py

--- mailScraper.py
+++ mailScraper.py
@@ -7,8 +7,8 @@ from email.mime.text import MIMEText
 i = 1
 news = ""
 r = requests.get("https://www.spieletipps.de/news/1/")
-soup = BeautifulSoup(r.text, 'html.parser')
-result = soup.find_all('h2', attrs={'class': 'component-headline teaser-headline'})
+soup = BeautifulSoup(r.text, "html.parser")
+result = soup.find_all("h2", attrs={"class": "component-headline teaser-headline"})
 
 for new in result:
     news += ("%s - \t " % i)
@@ -26,9 +26,9 @@ for new in result:
 #news = news.replace("è", "e")
 
 
-user = ''  # Enter your Email address
-password = ''  # Enter your Email pasword
-to = ''  # Enter the Recipient
+user = ""  # Enter your Email address
+password = ""  # Enter your Email pasword
+to = ""  # Enter the Recipient
 
 msg = MIMEMultipart("alternative")
 msg.set_charset("utf-8")
@@ -37,10 +37,10 @@ msg["Subject"] = "News Summary"
 msg["From"] = user
 msg["To"] = to
 
-msg.attach(MIMEText(news, 'plain'))#the message is attached here
+msg.attach(MIMEText(news, "plain"))#the message is attached here
 
 try:
-    server = smtplib.SMTP_SSL('mail.gmx.net', 465) #preset for GMX mail accounts
+    server = smtplib.SMTP_SSL("mail.gmx.net", 465) #preset for GMX mail accounts
     server.ehlo()
     server.login(user, password)
     server.sendmail(user, [to], msg.as_string())
