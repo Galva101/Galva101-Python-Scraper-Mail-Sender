b715e04a5b8305a352700ccc77290ba87906d01c
Roman Stadler <rosta99x@gmx.ch>
2020-07-25 14:10:23 +0200

    added the option for multiple recipients

--- mailScraper.py
+++ mailScraper.py
@@ -11,12 +11,14 @@ soup = BeautifulSoup(r.text, "html.parser")
 result = soup.find_all("h2", attrs={"class": "component-headline teaser-headline"})
 
 for new in result:
-    news += ("%s - \t " % i)
     new = new.text.strip()
     new = new.replace("\n", " ")
-    news += new
-    news += "\n"
-    i += 1
+    new = new.replace("\t", "")
+    if not len(new)<=15: #Minimum Headline Length
+        news += ("%s - \t " % i)
+        news += new
+        news += "\n"
+        i += 1
 
 #news = news.replace("ö", "oe")
 #news = news.replace("ä", "ae")
@@ -25,17 +27,17 @@ for new in result:
 #news = news.replace("é", "e")
 #news = news.replace("è", "e")
 
-
 user = ""  # Enter your Email address
 password = ""  # Enter your Email pasword
-to = ""  # Enter the Recipient
+to = recipients = ["", ]  # Enter the Recipients
+subject = "News Summary"
 
 msg = MIMEMultipart("alternative")
 msg.set_charset("utf-8")
 
-msg["Subject"] = "News Summary"
+msg["Subject"] = subject
 msg["From"] = user
-msg["To"] = to
+msg["To"] = ", ".join(to)
 
 msg.attach(MIMEText(news, "plain"))#the message is attached here
 
@@ -43,9 +45,8 @@ try:
     server = smtplib.SMTP_SSL("mail.gmx.net", 465) #preset for GMX mail accounts
     server.ehlo()
     server.login(user, password)
-    server.sendmail(user, [to], msg.as_string())
+    server.sendmail(user, to, msg.as_string())
     server.close()
     print("email sent")
 except:
     print("error while sending")
-
