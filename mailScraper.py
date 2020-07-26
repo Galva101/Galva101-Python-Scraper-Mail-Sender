3d57cae07af3f6ca6373040d60dcc108b4a84498
Roman Stadler <rosta99x@gmx.ch>
2020-07-26 19:09:19 +0200

    Update mailScraper.py
    added the option to specify a sender name, on top of the email address

--- mailScraper.py
+++ mailScraper.py
@@ -4,34 +4,35 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-i = 1
-news = ""
 r = requests.get("https://www.spieletipps.de/news/1/")
 soup = BeautifulSoup(r.text, "html.parser")
-result = soup.find_all("h2", attrs={"class": "component-headline teaser-headline"})
+headlines = soup.find_all("h2", attrs={"class": "component-headline teaser-headline"})
+a = soup.find_all('a', href=True) #finding all "href" links for the articles
 
-for new in result:
+links = []
+for a in a:
+    links.append(a['href']) ##append all link endings for the url into a new list
+links = links[12:] #remove all leading and trailing links not related to articles
+links = links[:-12]
+
+i = 1
+news = ""
+
+for new in headlines:
+    news += ("%s - \t " % i)
     new = new.text.strip()
     new = new.replace("\n", " ")
-    new = new.replace("\t", "")
-    if not len(new)<=15: #Minimum Length for a Headline
-        news += ("%s - \t " % i)
-        news += new
+    news += new + " https://www.spieletipps.de{}".format(links[i-1])
+    news += "\n"
+    if (i%5==0):
         news += "\n"
-        i += 1
-
-#news = news.replace("ö", "oe")
-#news = news.replace("ä", "ae")
-#news = news.replace("ü", "ue")
-#news = news.replace("ß", "ss")
-#news = news.replace("é", "e")
-#news = news.replace("è", "e")
+    i += 1
 
 user = ""  # Enter the Email address that will send the text
 name = "ScraPy" # Enter the name that will be displayed in the recipient's inbox
 password = ""  # Enter your Email pasword
 to = ["", ]  # Enter the Recipients
-subject = "News Update"
+subject = "Spieletipps Summary"
 
 msg = MIMEMultipart("alternative")
 msg.set_charset("utf-8")
@@ -51,3 +52,4 @@ try:
     print("email sent")
 except:
     print("error while sending")
+
